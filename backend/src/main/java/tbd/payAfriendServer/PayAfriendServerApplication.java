283b4af7c786ef7261802c18a3d79c59c507ea1c
Dominik van de Vathorst <Dominik.vandevathorst@outlook.com>
2023-10-20 16:12:15 +0200

    init spring

---
+++ backend/src/main/java/tbd/payAfriendServer/PayAfriendServerApplication.java
@@ -0,0 +1,13 @@
+package tbd.payAfriendServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayAfriendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayAfriendServerApplication.class, args);
+	}
+
+}
