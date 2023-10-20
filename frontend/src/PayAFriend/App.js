6f6abd0d9408c341c41b8965ac2000ed4a19eec5
Sebastian Eggers <sebastian.eggers@icloud.com>
2023-10-20 13:27:43 +0200

    Added react native basic project

---
+++ frontend/src/PayAFriend/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>PayAFriend</Text>
+      <View>
+        <Text>Text 2</Text>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
