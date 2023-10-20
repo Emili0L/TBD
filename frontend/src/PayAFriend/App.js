65f5bc2f0361a9584f7d35da01bb977a280fd76f
Sebastian Eggers <sebastian.eggers@icloud.com>
2023-10-20 14:17:24 +0200

    Added multi screen support

--- frontend/src/PayAFriend/App.js
+++ frontend/src/PayAFriend/App.js
@@ -1,16 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View , Button} from 'react-native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>PayAFriend</Text>
-      <View>
-        <Text>Text 2</Text>
-      </View>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="Welcome" component={WelcomeScreen} />
+          </Stack.Navigator>
+      </NavigationContainer>
   );
+}
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text>PayAFriend</Text>
+            <View>
+                <Text>iOS > Android</Text>
+                <Button title={"Willkommen"} onPress={() => navigation.navigate("Welcome")} />
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
+}
+
+const WelcomeScreen = ({navigation}) => {
+    return (
+        <View style={styles.container}>
+            <Text>Willkommen</Text>
+            <Button title={"Zurück"} onPress={() => navigation.navigate("Home")} />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
