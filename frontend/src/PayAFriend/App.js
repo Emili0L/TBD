a4c126e3b0386d4dc0347e4e8098200596f21016
Sebastian Eggers <sebastian.eggers@icloud.com>
2023-10-21 17:05:38 +0200

    Payment Screen

--- frontend/src/PayAFriend/App.js
+++ frontend/src/PayAFriend/App.js
@@ -1,19 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet, Text, TextInput, View , Button, Alert} from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View , Button, Alert, Dimensions, TouchableOpacity, ScrollView } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
 import LoginPage from './components/LoginPage/index.js'
+import * as Linking from 'expo-linking';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+    const prefix = Linking.createURL("/");
+    const config = {
+        screens: {
+            Payment: "payment/:userId"
+        }
+    }
+    const linking = {
+        prefixes: [prefix, "https://payafriend.ggrs.eu", "payafriend://"],
+        config
+    };
   return (
-      <NavigationContainer>
-          <Stack.Navigator>
+      <NavigationContainer linking={linking}>
+          <Stack.Navigator
+              initialRouteName="Home"
+              screenOptions={{
+                  cardOverlayEnabled: true,
+                  headerShown: false,
+                  presentation: "modal",
+              }}
+          >
             <Stack.Screen name="Login" component={LoginPage} />
             <Stack.Screen name="Welcome" component={WelcomeScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Payment" component={PaymentScreen} />
           </Stack.Navigator>
       </NavigationContainer>
   );
@@ -49,6 +68,9 @@ const HomeScreen = ({navigation}) => {
                     value={password}
                 />
                 <Button title={"Next"} onPress={() => navigation.navigate("Welcome")} />
+                <Button title="Bezahlen" onPress={() => navigation.navigate("Payment", {
+                    userId: "Test"
+                })} />
             </View>
             <StatusBar style="auto" />
         </View>
@@ -63,6 +85,57 @@ const WelcomeScreen = ({navigation}) => {
         </View>
     )
 }
+const PaymentScreen = ({route, navigation}) => {
+    const [poolData, setPoolData] = useState({
+        receiverId: null,
+        receiverName: "Loading",
+        receiverPicture: null,
+        paymentAmount: 999,
+    });
+    const [modalHeight, setModalHeight] = useState(0);
+    const handleAmountChange = (value) => {
+        setPoolData(({ ...poolData, paymentAmount: !isNaN(parseFloat(value)) ? parseFloat(value): 0 }));
+    }
+
+    const { userId } = route.params;
+
+    return (
+        <ScrollView
+            keyboardShouldPersistTaps="handled"
+            onLayout={(event) => {
+                const { height } = event.nativeEvent.layout;
+                setModalHeight(height);
+            }}
+        >
+            <View style={{...styles.containerPayment, height: modalHeight}}>
+                <TouchableOpacity style={styles.closeButton} onPress={() => { navigation.navigate("Home") }}>
+                    <Text style={styles.closeButtonText}>Abbrechen</Text>
+                </TouchableOpacity>
+                {
+                    poolData.receiverPicture != null ? <></> : <View style={styles.profileImagePlaceholder}></View>
+                }
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <TextInput
+                        style={styles.amount}
+                        value={`${poolData.paymentAmount}`}
+                        onChangeText={handleAmountChange}
+                        keyboardType="numeric"
+                        maxLength={4}
+                        selectTextOnFocus={true}
+                        selectionColor="transparent"
+                        underlineColorAndroid="transparent"
+                    />
+                    <Text style={styles.euroSymbol}>€</Text>
+                </View>
+                <TouchableOpacity style={styles.sendButton} onPress={() => { navigation.navigate("Home") }}>
+                    <Text style={styles.sendButtonText}>An {poolData.receiverName} senden</Text>
+                </TouchableOpacity>
+            </View>
+        </ScrollView>
+    )
+}
+
+const { width, height } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
@@ -77,5 +150,54 @@ const styles = StyleSheet.create({
     fontSize: 32,
     fontStyle: "normal",
     fontWeight: "600",
-  }
+  },
+    containerPayment: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-evenly'
+    },
+    profileImagePlaceholder: {
+        width: width * 0.6,
+        height: width * 0.6,
+        borderRadius: width * 0.3,
+        backgroundColor: '#eee',
+        marginTop: 50
+    },
+    amount: {
+        fontSize: 56,
+        fontWeight: 'bold',
+        marginVertical: 20
+    },
+    euroSymbol: {
+        marginLeft: 5,
+        fontSize: 56,
+        fontWeight: 'bold'
+    },
+    sendButton: {
+        width: width * 0.6,
+        height: 50,
+        backgroundColor: '#002E3F',
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 50
+    },
+    sendButtonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: 'rgba(0,0,0,0.1)',
+    },
+    closeButtonText: {
+        color: '#000',
+        fontSize: 16,
+    }
 });
